63322c2ea66c967539b696c3c634c26fcfd6a43f
ngvantha <ngvantha@users.noreply.github.com>
2019-05-14 14:24:16 +0700

    entityframework

---
+++ TeduShop/TeduShop.Data/Infratructure/DbFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace TeduShop.Data.Infratructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private TeduShopDbContext dbContext;
+
+        public TeduShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new TeduShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
--- TeduShop/TeduShop.Data/Infratructure/IDbFactory.cs
+++ TeduShop/TeduShop.Data/Infratructure/IDbFactory.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TeduShop.Data
+namespace TeduShop.Data.Infratructure
 {
-    public class Class1
+    public interface IDbFactory: IDisposable
     {
+        TeduShopDbContext Init();
     }
 }
