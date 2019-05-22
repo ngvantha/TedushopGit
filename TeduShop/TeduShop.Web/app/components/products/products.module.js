943b2f54ea11b8f380fca29fe6d83c30e9904b68
ngvantha <ngvantha@users.noreply.github.com>
2019-05-22 18:32:32 +0700

    angular config

---
+++ TeduShop/TeduShop.Web/app/components/products/products.module.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    angular.module('tedushop.products', ['tedushop.common']).config(config);
+    config.$injec = ['$stateProvider', '$urlRouterProvider'];
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('products', {
+            url: "/products",
+            templateUrl: "/app/components/products/productListView.html",
+            controller: "productListController"
+        }).state('products_add', {
+            url: "/products_add",
+            templateUrl: "/app/components/products/productAddView.html",
+            controller: "productAddController"
+        });
+
+    }
+})();
---
+++ TeduShop/TeduShop.Web/app/shared/modules/tedushop.Common.js
@@ -0,0 +1,4 @@
+﻿(function () {
+    angular.module('tedushop.common', ['ui.router']);
+    
+})();
